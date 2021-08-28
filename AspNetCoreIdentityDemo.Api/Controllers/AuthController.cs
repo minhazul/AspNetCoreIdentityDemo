1a072b4c0620b097cd34ac597e7ecd18e60d2320
minhazulislam <minhazcste13@gmail.com>
2021-08-28 19:07:05 +0600

    Add project files.

---
+++ AspNetCoreIdentityDemo.Api/Controllers/AuthController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspNetCoreIdentityDemo.Api.Services;
+using AspNetCoreIdentityDemo.Shared;
+
+namespace AspNetCoreIdentityDemo.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+
+        private IUserService _userService;
+        private IMailService _mailService;
+
+        public AuthController(IUserService userService, IMailService mailService)
+        {
+            _userService = userService;
+            _mailService = mailService;
+        }
+        // /api/auth/register
+        [HttpPost("Register")]
+        public async Task<IActionResult> RegisterAsync([FromBody]RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _userService.RegisterUserAsync(model);
+
+                if (result.IsSuccess)
+                {
+                    return Ok(result);
+                }
+
+                return BadRequest(result);
+            }
+
+            return BadRequest("Some properties are not valid now");
+        }
+
+        [HttpPost("Login")]
+        public async Task<IActionResult> LoginAsync([FromBody] LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _userService.LoginUserAsync(model);
+
+                if (result.IsSuccess)
+                {
+                    await _mailService.SendEmailAsync(model.Email, "New Login",
+                        "<h1>Hey!, new login to your account noticed</h1><p>New login to your account at </p>" +
+                        DateTime.Now);
+                    return Ok(result);
+                }
+
+                return BadRequest(result);
+            }
+
+            return BadRequest("Some properties are not valid");
+        }
+    }
+}
---
+++ AspNetCoreIdentityDemo.Api/Services/IUserService.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using AspNetCoreIdentityDemo.Shared;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+namespace AspNetCoreIdentityDemo.Api.Services
+{
+    public interface IUserService
+    {
+        Task<UserManagerResponse> RegisterUserAsync(RegisterViewModel model);
+        Task<UserManagerResponse> LoginUserAsync(LoginViewModel model);
+    }
+
+    public class UserService : IUserService
+    {
+        private UserManager<IdentityUser> _userManager;
+        private IConfiguration _configuration;
+
+        public UserService(UserManager<IdentityUser> userManager, IConfiguration configuration)
+        {
+            _userManager = userManager;
+            _configuration = configuration;
+        }
+
+        public async Task<UserManagerResponse> RegisterUserAsync(RegisterViewModel model)
+        {
+            if (model == null)
+            {
+                throw new NullReferenceException("Register Model is null");
+            }
+
+            if (model.Password != model.ConfirmPassword)
+            {
+                return new UserManagerResponse
+                {
+                    Message = "Confirm password doesn't match the password",
+                    IsSuccess = false
+                };
+            }
+
+            var identityUser = new IdentityUser
+            {
+                Email = model.Email,
+                UserName = model.Email
+            };
+
+            var result = await _userManager.CreateAsync(identityUser, model.Password);
+
+            if (result.Succeeded)
+            {
+                return new UserManagerResponse
+                {
+                    Message = "Üser created successfully",
+                    IsSuccess = true
+                };
+            }
+
+            return new UserManagerResponse
+            {
+                Message = "User did not create",
+                IsSuccess = false,
+                Errors = result.Errors.Select(e => e.Description)
+            };
+        }
+
+        public async Task<UserManagerResponse> LoginUserAsync(LoginViewModel model)
+        {
+            var user = await _userManager.FindByEmailAsync(model.Email);
+
+            if (user == null)
+            {
+                return new UserManagerResponse
+                {
+                    Message = "There is no user with tgat Email address",
+                    IsSuccess = false
+                };
+            }
+
+            var result = await _userManager.CheckPasswordAsync(user, model.Password);
+
+            if (!result)
+            {
+                return new UserManagerResponse
+                {
+                    Message = "Invalid Password",
+                    IsSuccess = false
+                };
+            }
+
+            var claims = new[]
+            {
+                new Claim("Email", model.Email),
+                new Claim(ClaimTypes.NameIdentifier, user.Id)
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["AuthSettings:Key"]));
+
+            var token = new JwtSecurityToken(
+                issuer: _configuration["AuthSettings:Issuer"],
+                audience: _configuration["AuthSettings: Audience"],
+                claims: claims,
+                expires: DateTime.Now.AddDays(30),
+                signingCredentials: new SigningCredentials(key, SecurityAlgorithms.HmacSha256));
+
+            string tokenAsString = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return new UserManagerResponse
+            {
+                Message = tokenAsString,
+                IsSuccess = true,
+                ExpireDate = token.ValidTo
+            };
+        }
+    }
+}
